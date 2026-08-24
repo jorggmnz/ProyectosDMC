4f8011297f07e7f2d3a840ae300aabb7b0b11fc9
jorggmnz <jorgemnz2.cc@gmail.com>
2026-08-23 22:21:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ import pandas as pd
 import librería_funciones_proyecto1 as lf
 
 
-  st.title("Proyecto aplicado N°1")
-  st.write("Módulo: N°1 de Python Fundamentals")
-  st.write("Año: 2026")
+st.title("Proyecto aplicado N°1")
+st.write("Módulo: N°1 de Python Fundamentals")
+st.write("Año: 2026")
 
 # Separador visual
-  st.divider()
+st.divider()
 
-  st.image("Python_logo.png", width=200)
-  st.sidebar.image("DMC.png", width=100)
+st.image("Python_logo.png", width=200)
+st.sidebar.image("DMC.png", width=100)
 
 modulos = st.sidebar.selectbox ("Selecione un módulo", ["Home","Ejercicio 1: Listas", "Ejercicio 2: Arreglos con Numpy", "Ejercicio 3: Funciones", "Ejercicio 4: CRUD"])
 
